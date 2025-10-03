f2688533dd19a5608fe8521f70b56646fb4c5123
Ridha Hakeem <ridhahakeem09@gmail.com>
2025-10-03 11:40:58 +0530

    Task18

---
+++ Task18/Task18/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudApp.Models;
+
+namespace CrudApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static List<Employee> employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "John", Position = "Developer" },
+            new Employee { Id = 2, Name = "Jane", Position = "Designer" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            employee.Id = employees.Max(e => e.Id) + 1;
+            employees.Add(employee);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var emp = employees.FirstOrDefault(e => e.Id == id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee updatedEmployee)
+        {
+            var emp = employees.FirstOrDefault(e => e.Id == updatedEmployee.Id);
+            if (emp != null)
+            {
+                emp.Name = updatedEmployee.Name;
+                emp.Position = updatedEmployee.Position;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = employees.FirstOrDefault(e => e.Id == id);
+            if (emp != null) employees.Remove(emp);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Task18/Task18/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudApp.Models;
+
+namespace CrudApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> users = new List<User>
+        {
+            new User { Id = 1, Name = "Alice", Email = "alice@mail.com" },
+            new User { Id = 2, Name = "Bob", Email = "bob@mail.com" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            user.Id = users.Max(u => u.Id) + 1;
+            users.Add(user);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User updatedUser)
+        {
+            var user = users.FirstOrDefault(u => u.Id == updatedUser.Id);
+            if (user != null)
+            {
+                user.Name = updatedUser.Name;
+                user.Email = updatedUser.Email;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if (user != null) users.Remove(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
